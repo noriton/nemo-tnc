b605227e1eef43b1f9f7701f526c5407a873f740
Norito Nemoto <noriton@gmail.com>
2026-02-13 20:01:54 +0900

    buufer name change

--- main/tnc_buffer.h
+++ main/tnc_buffer.h
@@ -4,7 +4,8 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/ringbuf.h"
 
-extern RingbufHandle_t usb_rb[2]; // 配列にしておくと管理が楽です
+extern RingbufHandle_t usb_from_pc[2]; // PC -> TNC
+extern RingbufHandle_t usb_to_pc[2];   // TNC -> PC
 extern RingbufHandle_t rx_ringbuf[2];
 extern RingbufHandle_t tx_ringbuf[2];
 
